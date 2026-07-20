fa8db632ec11b99995e5cb7c6cf2c66c532822f5
murer <paulo.murer@infinity6.ai>
2026-07-20 16:58:19 -0300

    x

--- commonz/ioz/iobytez/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package iobytez provides enhanced I/O functionality, focusing on reading from io.Reader with fine-grained control.
-
-The package is designed to address common challenges in I/O operations, such as reading a specific number of bytes, setting limits on the data size, and handling reads with context-based timeouts and cancellations. The primary function, Read, offers a flexible way to manage byte streams, making it a versatile tool for network programming, file processing, and other I/O-intensive tasks.
-*/
-package iobytez
