29fee880b8cf86657eafabfc9f76f122cb5cf22e
murer <paulo.murer@infinity6.ai>
2026-07-20 15:34:06 -0300

    x

--- commonz/ioz/iobytez/iobytez_read.go
+++ commonz/ioz/iobytez/iobytez_read.go
@@ -1,41 +1,34 @@
 package iobytez
 
 import (
+	"bytes"
 	"context"
 	"io"
 )
 
 const (
-	// As per test TestUnitRead_Exact, it expects a capacity of 100
-	// for a new buffer when reading a small number of bytes.
-	defaultBufferCap = 100
+	defaultBufferCap = 4096
 )
 
-// Options sets the parameters for the Read function.
 type Options struct {
-	// Out is the buffer to which the data will be appended.
-	// If nil, a new buffer will be allocated.
-	Out []byte
-	// Min is the minimum number of bytes to read.
-	// If the reader returns an EOF before Min bytes are read, io.ErrUnexpectedEOF is returned.
 	Min int
-	// Max is the maximum number of bytes to read.
-	// The read will stop once Max bytes are read.
 	Max int
+	Out *bytes.Buffer
 }
 
-// Clean resets the output buffer to be reused.
-func (o *Options) Clean() *Options {
+func (o *Options) fix() {
 	if o.Out == nil {
-		o.Out = make([]byte, 0, defaultBufferCap)
-		return o
+		c := defaultBufferCap
+		if c < o.Min {
+			c = o.Min
+		}
+		if o.Max > 0 && c > o.Max {
+			c = o.Max
+		}
+		o.Out = bytes.NewBuffer(make([]byte, c))
 	}
-	o.Out = o.Out[:0]
-	return o
 }
 
-// Read reads from r into o.Out until at least o.Min bytes have been read or o.Max bytes have been read.
-// Min and Max are relative to the number of bytes read in this call, not the total size of Out.
 func Read(ctx context.Context, r io.Reader, opts *Options) error {
 	select {
 	case <-ctx.Done():
@@ -43,76 +36,5 @@ func Read(ctx context.Context, r io.Reader, opts *Options) error {
 	default:
 	}
 
-	startLen := len(opts.Out)
-	if opts.Out == nil {
-		capacity := opts.Max
-		if capacity < defaultBufferCap {
-			capacity = defaultBufferCap
-		}
-		opts.Out = make([]byte, 0, capacity)
-	}
-
-	if opts.Max <= 0 {
-		if opts.Min > 0 {
-			return io.ErrUnexpectedEOF
-		}
-		return nil
-	}
-
-	var lastErr error
-	for {
-		bytesRead := len(opts.Out) - startLen
-		if bytesRead >= opts.Max {
-			break
-		}
-
-		neededCap := startLen + opts.Max
-		if cap(opts.Out) < neededCap {
-			newCap := neededCap
-			if newCap < cap(opts.Out)+defaultBufferCap {
-				newCap = cap(opts.Out) + defaultBufferCap
-			}
-			newBuf := make([]byte, len(opts.Out), newCap)
-			copy(newBuf, opts.Out)
-			opts.Out = newBuf
-		}
-
-		readSlice := opts.Out[len(opts.Out):cap(opts.Out)]
-		if len(readSlice) > (opts.Max - bytesRead) {
-			readSlice = readSlice[:(opts.Max - bytesRead)]
-		}
-
-		n, err := r.Read(readSlice)
-		if n > 0 {
-			opts.Out = opts.Out[:len(opts.Out)+n]
-		}
-
-		if err != nil {
-			lastErr = err
-			break
-		}
-		if n == 0 {
-			break
-		}
-	}
-
-	bytesRead := len(opts.Out) - startLen
-	if lastErr == io.EOF {
-		if bytesRead < opts.Min {
-			if startLen == 0 && bytesRead == 0 && opts.Min > 0 {
-				return io.EOF
-			}
-			return io.ErrUnexpectedEOF
-		}
-		if bytesRead == 0 {
-			return io.EOF
-		}
-		return nil
-	}
-
-	if bytesRead < opts.Min {
-		return io.ErrUnexpectedEOF
-	}
-
-	return nil
+	opts.fix()
 }
