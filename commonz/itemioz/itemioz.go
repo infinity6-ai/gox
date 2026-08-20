72974f65d2f66391bcd5a9fd7370c9575fabec92
murer <paulo.murer@infinity6.ai>
2026-08-20 18:59:13 -0300

    x

---
+++ commonz/itemioz/itemioz.go
@@ -0,0 +1,14 @@
+package itemioz
+
+type ItemReader[T any] interface {
+	ReadItem() (*T, error)
+}
+
+type ItemWriter[T any] interface {
+	WriteItem(item *T) error
+}
+
+type ItemReaderWriter[T any] interface {
+	ItemReader[T]
+	ItemWriter[T]
+}
