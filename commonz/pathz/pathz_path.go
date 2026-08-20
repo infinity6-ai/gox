670b0e7a12cc7688447423907627be02f03e0708
murer <paulo.murer@infinity6.ai>
2026-08-20 16:01:23 -0300

    x

--- commonz/pathz/pathz_path.go
+++ commonz/pathz/pathz_path.go
@@ -89,3 +89,19 @@ func Parse(input string) (*Path, error) {
 		HasEndingSlash: hasEndingSlash,
 	}, nil
 }
+
+func IsValidChar(r rune) bool {
+	if r >= 'a' && r <= 'z' {
+		return true
+	}
+	if r >= 'A' && r <= 'Z' {
+		return true
+	}
+	if r >= '0' && r <= '9' {
+		return true
+	}
+	if r == '-' || r == '_' || r == '.' || r == '/' || r == '=' || r == '{' || r == '}' {
+		return true
+	}
+	return false
+}
