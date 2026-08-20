116db75b28fb24d96b44f380344b08d82d739d35
murer <paulo.murer@infinity6.ai>
2026-08-20 13:52:45 -0300

    x

--- commonz/jsonz/jsonz.go
+++ commonz/jsonz/jsonz.go
@@ -10,10 +10,10 @@ func MustParse[T any, I blobz.Data](data I) *T {
 	panic("implement it")
 }
 
-func Format[T any, O blobz.Data](data *T) (O, error) {
+func Format[T any](data *T) (blobz.Blob, error) {
 	panic("implement it")
 }
 
-func MustFormat[T any, O blobz.Data](data *T) O {
+func MustFormat[T any](data *T) blobz.Blob {
 	panic("implement it")
 }
