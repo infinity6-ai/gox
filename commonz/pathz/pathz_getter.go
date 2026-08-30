659147eddb350c1c94a6f56bb348fdcc423c2025
murer <paulo.murer@infinity6.ai>
2026-08-30 09:23:09 -0300

    x

--- commonz/pathz/pathz_getter.go
+++ commonz/pathz/pathz_getter.go
@@ -1,1 +1,13 @@
 package pathz
+
+func (p *Path) IsEscaped() bool {
+	return p.parents != 0
+}
+
+func (p *Path) IsAbsolute() bool {
+	return p.parents == -1
+}
+
+func (p *Path) IsContained() bool {
+	return p.parents == 0
+}
