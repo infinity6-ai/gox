a043ebfa38c7b593cb138614155077b8c0c46afc
murer <paulo.murer@infinity6.ai>
2026-08-31 14:39:24 -0300

    x

---
+++ commonz/logz/logzevent/logzevent.go
@@ -0,0 +1,80 @@
+package logzevent
+
+import (
+	"context"
+	"sync"
+)
+
+type Logger interface {
+	Error(ctx context.Context, op string, params map[string]any, errs ...error)
+	Info(ctx context.Context, op string, params map[string]any, errs ...error)
+	Debug(ctx context.Context, op string, params map[string]any, errs ...error)
+	Appender() string
+}
+
+func New(l Logger) Logger {
+	return &LoggerEvent{
+		logger: l,
+	}
+}
+
+type Level string
+
+const (
+	LevelDebug Level = "debug"
+	LevelInfo  Level = "info"
+	LevelError Level = "error"
+)
+
+type Event struct {
+	Level  Level
+	Op     string
+	Params map[string]any
+	Errs   []error
+}
+
+type LoggerEvent struct {
+	logger Logger
+	events []*Event
+	mu     sync.RWMutex
+}
+
+func (l *LoggerEvent) add(event Event) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if len(l.events) > 5 {
+		l.events = l.events[1:]
+	}
+	l.events = append(l.events, &event)
+}
+
+func (l *LoggerEvent) Appender() string {
+	return l.logger.Appender()
+}
+
+func (l *LoggerEvent) Debug(ctx context.Context, op string, params map[string]any, errs ...error) {
+	l.add(Event{
+		Level:  LevelDebug,
+		Op:     op,
+		Params: params,
+		Errs:   errs,
+	})
+}
+
+func (l *LoggerEvent) Error(ctx context.Context, op string, params map[string]any, errs ...error) {
+	l.add(Event{
+		Level:  LevelError,
+		Op:     op,
+		Params: params,
+		Errs:   errs,
+	})
+}
+
+func (l *LoggerEvent) Info(ctx context.Context, op string, params map[string]any, errs ...error) {
+	l.add(Event{
+		Level:  LevelInfo,
+		Op:     op,
+		Params: params,
+		Errs:   errs,
+	})
+}
