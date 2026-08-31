af695f4a8623c116d7b85f4b7319392c646dd4ac
murer <paulo.murer@infinity6.ai>
2026-08-31 12:06:31 -0300

    x

--- commonz/validation/validation_comprable.go
+++ commonz/validation/validation_comprable.go
@@ -17,7 +17,7 @@ func NotEqual[T comparable](expected T, actual T, msg string, args ...any) error
 }
 
 func OneOf[T comparable](expected []T, actual T, msg string, args ...any) error {
-	if slices.Contains(expected, actual) {
+	if !slices.Contains(expected, actual) {
 		return newError("must be one of", map[string]any{"expected": expected, "actual": actual}, msg, args...)
 	}
 	return nil
