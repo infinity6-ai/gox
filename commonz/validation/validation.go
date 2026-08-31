15cafd38f3edc73be71fd2190b4348264ad701fe
murer <paulo.murer@infinity6.ai>
2026-08-31 10:22:12 -0300

    x

--- commonz/validation/validation.go
+++ commonz/validation/validation.go
@@ -3,6 +3,8 @@ package validation
 import (
 	"errors"
 	"fmt"
+
+	"golang.org/x/exp/constraints"
 )
 
 var ErrValidation = errors.New("validation error")
@@ -28,16 +30,15 @@ func Fail(msg string, args ...any) error {
 	return newError("fail", nil, msg, args...)
 }
 
-// func Equal[T comparable](expected T, actual T, msg string, args ...any) error {
-// 	if expected != actual {
-// 		return errorf("expected %v, got %v", []any{expected, actual}, msg, args...)
-// 	}
-// 	return nil
-// }
+func Equal[T comparable](expected T, actual T, msg string, args ...any) error {
+	if expected != actual {
+		return newError("must be equal", map[string]any{"expected": expected, "actual": actual}, msg, args...)
+	}
+	return nil
+}
 
-// func Greater[T constraints.Ordered](value, threshold T, msg string, args ...any) {
-// 	if value <= threshold {
-// 		errorf("expected %v to be greater than %v", []any{value, threshold}, msg, args...)
-// 		// util.Panic("validation fail Greater", map[string]any{"msg": fmt.Sprintf(msg, args...), "threshold": threshold, "actual": value})
-// 	}
-// }
+func Greater[T constraints.Ordered](value, threshold T, msg string, args ...any) error {
+	if value <= threshold {
+		return newError("must be greater than", map[string]any{"threshold": threshold, "actual": value}, msg, args...)
+	}
+}
