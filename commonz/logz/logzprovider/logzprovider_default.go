f56a32ceb14c0a01d8daf5431a5e682298b1cf72
murer <paulo.murer@infinity6.ai>
2026-08-31 14:16:36 -0300

    x

--- commonz/logz/logzprovider/logzprovider_default.go
+++ commonz/logz/logzprovider/logzprovider_default.go
@@ -31,23 +31,3 @@ func SetDefaultProvider(provider Provider) {
 func GetDefaultProvider() Provider {
 	return defaultProvider
 }
-
-type SimpleProvider struct {
-	appender string
-}
-
-func (s *SimpleProvider) Appender() string {
-	return s.appender
-}
-
-func (s *SimpleProvider) Debug(ctx context.Context, op string, params map[string]any, errs ...error) {
-	panic("unimplemented")
-}
-
-func (s *SimpleProvider) Error(ctx context.Context, op string, params map[string]any, errs ...error) {
-	panic("unimplemented")
-}
-
-func (s *SimpleProvider) Info(ctx context.Context, op string, params map[string]any, errs ...error) {
-	panic("unimplemented")
-}
