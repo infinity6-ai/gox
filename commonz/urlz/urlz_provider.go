e64f7832ce312e6be30e9bf77a20e03b06d98a95
murer <paulo.murer@infinity6.ai>
2026-08-31 09:40:58 -0300

    x

--- commonz/urlz/urlz_provider.go
+++ commonz/urlz/urlz_provider.go
@@ -2,7 +2,10 @@ package urlz
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
+
+	"github.com/infinity6-ai/gox/commonz/pathz"
 )
 
 var ErrUnknownScheme = errors.New("unknown scheme")
@@ -13,6 +16,7 @@ type providerSpec struct {
 }
 
 func getProvider(scheme string) (*providerSpec, error) {
+	prv := &providerSpec{}
 	switch scheme {
 	case "http", "https":
 		panic("implement")
@@ -21,8 +25,22 @@ func getProvider(scheme string) (*providerSpec, error) {
 	case "gs":
 		panic("implement")
 	case "unix":
-		panic("implement")
+		return prv.providerUnix()
 	default:
 		return nil, ErrUnknownScheme
 	}
+}
+
+func (p *providerSpec) providerUnix() (*providerSpec, error) {
+	p.Parser = func(u *url.URL) (*Url, error) {
+		pt, err := pathz.Parse(u.Path)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing unix url: %s, %w", u, err)
+		}
+		return &Url{Scheme: u.Scheme, Path: pt}, nil
+	}
+	p.ToString = func(u *Url) string {
+		return u.Scheme + ":/" + u.Path.String()
+	}
+	return p, nil
 }
