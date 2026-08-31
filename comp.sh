2076d1235c159ec9617cfc5c64d29070f44fa423
murer <paulo.murer@infinity6.ai>
2026-08-31 13:33:41 -0300

    x

--- comp.sh
+++ comp.sh
@@ -40,7 +40,7 @@ function cmd_fmt() {
 function cmd_release() {
   cmd_codegen
   i6dev golang bin-compile
-  # i6dev golang release
+  i6dev golang release
   # i6dev golang bin-release latest
 }
 
