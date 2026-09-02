bf900a8a1224b2179e5e08aa02ec3fa2bc8fd6e8
murer <paulo.murer@infinity6.ai>
2026-09-02 14:12:36 -0300

    x

---
+++ commonz/regexpz/regexpzcomp/regexpzcomp.go
@@ -0,0 +1,57 @@
+package regexpzcomp
+
+import (
+	"fmt"
+	"regexp"
+	"sync"
+
+	"github.com/infinity6-ai/gox/commonz/errorz"
+)
+
+type Collection struct {
+	coll map[string]*regexp.Regexp
+	mu   sync.RWMutex
+}
+
+func New(buffer int) *Collection {
+	return &Collection{
+		coll: make(map[string]*regexp.Regexp, buffer),
+	}
+}
+
+var root = New(0)
+
+func (c *Collection) internalGet(pattern string) *regexp.Regexp {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.coll[pattern]
+}
+
+func (c *Collection) Get(pattern string, re *regexp.Regexp) (*regexp.Regexp, error) {
+	ret := c.internalGet(pattern)
+	if ret != nil {
+		return ret, nil
+	}
+	ret, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, fmt.Errorf("%w: error compiling regexp: %s", err, pattern)
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.coll[pattern] = ret
+	return ret, nil
+}
+
+func (c *Collection) Must(pattern string, re *regexp.Regexp) *regexp.Regexp {
+	ret, err := c.Get(pattern, re)
+	errorz.Check(err)
+	return ret
+}
+
+func Get(pattern string, re *regexp.Regexp) (*regexp.Regexp, error) {
+	return root.Get(pattern, re)
+}
+
+func Must(pattern string, re *regexp.Regexp) *regexp.Regexp {
+	return root.Must(pattern, re)
+}
