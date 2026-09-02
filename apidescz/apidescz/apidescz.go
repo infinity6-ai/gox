d3002b37ca28aa8232d32956f1c924fd168e525d
murer <paulo.murer@infinity6.ai>
2026-09-02 13:19:04 -0300

    x

--- apidescz/apidescz/apidescz.go
+++ apidescz/apidescz/apidescz.go
@@ -1,4 +1,14 @@
 package apidescz
 
 type Api[I any, O any] struct {
+	Name   string
+	Groups []string
+	Short  string
+	Guide  string
+	Path   string
+	Input  I
+	Output O
+}
+
+type Collection struct {
 }
