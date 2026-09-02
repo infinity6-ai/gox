9630a52cbdbf86cccb79808d7a28946f7f050256
murer <paulo.murer@infinity6.ai>
2026-09-02 14:37:55 -0300

    x

---
+++ commonz/regexpz/regexpz_engine.go
@@ -0,0 +1,59 @@
+package regexpz
+
+import (
+	"regexp"
+	"strings"
+)
+
+type Operation string
+
+const (
+	MatchOperation    Operation = "match"
+	MismatchOperation Operation = "mismatch"
+	ReplaceOperation  Operation = "replace"
+	SplitOperation    Operation = "split"
+	DeleteOperation   Operation = "delete"
+)
+
+type Rule struct {
+	Name      string
+	Regexp    *regexp.Regexp
+	Operation Operation
+	Out       string
+}
+
+func (r *Rule) Apply(s string) (string, bool) {
+	switch r.Operation {
+	case MatchOperation:
+		matched := r.Regexp.MatchString(s)
+		return s, matched
+	case MismatchOperation:
+		return s, false
+	case ReplaceOperation:
+		return r.Regexp.ReplaceAllString(s, r.Out), true
+	case SplitOperation:
+		splitted := r.Regexp.Split(s, -1)
+		return strings.Join(splitted, r.Out), true
+	case DeleteOperation:
+		return r.Regexp.ReplaceAllString(s, ""), true
+	default:
+		return s, false
+	}
+
+}
+
+type Engine struct {
+	Rules []Rule
+}
+
+func (e *Engine) Apply(s string) (string, bool) {
+	c := s
+	for _, rule := range e.Rules {
+		var ok bool
+		c, ok = rule.Apply(c)
+		if !ok {
+			return s, false
+		}
+	}
+	return c, true
+}
