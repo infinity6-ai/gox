47bdd41f5b8693d990c66bdc069685d265b17a0b
murer <paulo.murer@infinity6.ai>
2026-09-03 16:52:47 -0300

    x

--- commonz/jsonz/jsonz_parser.go
+++ commonz/jsonz/jsonz_parser.go
@@ -23,6 +23,28 @@ func Parse[T any, I blobz.Data](data I, v T) (T, error) {
 	return ParseReader(blobz.New(data).NewReader(), v)
 }
 
+// ParseReaderUntilEOF reads all JSON objects from a reader until it encounters an
+// io.EOF. This is useful for reading streams of JSON objects.
+func ParseReaderUntilEOF[T any](r io.Reader) ([]*T, error) {
+	result := make([]*T, 0)
+	decoder := json.NewDecoder(r)
+	decoder.UseNumber()
+
+	for {
+		var v T
+		err := decoder.Decode(&v)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse json stream: %w", err)
+		}
+		result = append(result, &v)
+	}
+
+	return result, nil
+}
+
 func FormatWriter(w io.Writer, v any) error {
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(v)
