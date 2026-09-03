fd6f21cc4f2cb5593e34aa54c8a3b36523a9fe6a
murer <paulo.murer@infinity6.ai>
2026-09-03 13:42:29 -0300

    x

---
+++ commonz/jsonz/jsonmapz/jsonmapz.go
@@ -0,0 +1,9 @@
+package jsonmapz
+
+func ParseMap(in map[string][]string, out map[string]any) error {
+	panic("implement it")
+}
+
+func FormatMap(in map[string]any, out map[string][]string) error {
+	panic("implement it")
+}
---
+++ commonz/jsonz/jsonmapz/jsonzmapz_test.go
@@ -0,0 +1,53 @@
+package jsonmapz_test
+
+import (
+	"testing"
+
+	"github.com/infinity6-ai/gox/commonz/errorz"
+	"github.com/infinity6-ai/gox/commonz/jsonz/jsonmapz"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPocHowTo(t *testing.T) {
+
+	type Data struct {
+		Id  string
+		Age int
+	}
+
+	out := map[string]any{
+		"a": "",
+		"b": []string{},
+		"c": float64(0),
+		"d": []float64{},
+		"e": &Data{},
+		"F": []*Data{},
+	}
+
+	unparsed := map[string][]string{
+		"a": {"a1", "a2"},
+		"b": {"b1", "b2"},
+		"c": {"10.2", "11.2"},
+		"d": {"20.2", "21.2"},
+		"e": {"{\"Id\":\"e1\", \"Age\":10}", "{\"Id\":\"e2\", \"Age\":11}"},
+		"F": {"{\"Id\":\"f1\", \"Age\":20}", "{\"Id\":\"f2\", \"Age\":21}"},
+	}
+
+	expected := map[string]any{
+		"a": "a1",
+		"b": []string{"b1", "b2"},
+		"c": 10.2,
+		"d": []float64{20.2, 21.2},
+		"e": &Data{Id: "e1", Age: 10},
+		"F": []*Data{
+			{Id: "f1", Age: 20},
+			{Id: "f2", Age: 21},
+		},
+	}
+
+	err := jsonmapz.ParseMap(unparsed, out)
+	errorz.Check(err)
+
+	assert.Equal(t, expected, out)
+
+}
