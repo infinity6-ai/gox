5845128a8d7e4f6a0d653f360531a197f496d666
murer <paulo.murer@infinity6.ai>
2026-09-03 09:45:36 -0300

    x

--- commonz/constraintz/parserz/parserz.go
+++ commonz/constraintz/parserz/parserz.go
@@ -1,5 +1,7 @@
 package parserz
 
+import "github.com/infinity6-ai/gox/commonz/errorz"
+
 type ItemReader[T any] interface {
 	ReadItem() (T, error)
 }
@@ -11,6 +13,19 @@ type ItemWriter[T any] interface {
 type ItemReaderWriter[T any] interface {
 	ItemReader[T]
 	ItemWriter[T]
+}
+
+type MustItemReader[T any] interface {
+	MustReadItem() T
+}
+
+type MustItemWriter[T any] interface {
+	MustWriteItem(item T)
+}
+
+type MustItemReaderWriter[T any] interface {
+	MustItemReader[T]
+	MustItemWriter[T]
 }
 
 type itemReaderWriter[T any] struct {
@@ -32,7 +47,25 @@ func (r *itemReaderWriter[T]) WriteItem(item T) error {
 	return r.write(item)
 }
 
+func (r *itemReaderWriter[T]) MustReadItem() T {
+	item, err := r.ReadItem()
+	errorz.Check(err)
+
+	return item
+}
+
+func (r *itemReaderWriter[T]) MustWriteItem(item T) {
+	errorz.Check(r.WriteItem(item))
+}
+
 func NewItemReaderWriter[T any](reader func() (T, error), writer func(item T) error) ItemReaderWriter[T] {
+	return &itemReaderWriter[T]{
+		read:  reader,
+		write: writer,
+	}
+}
+
+func NewMustItemReaderWriter[T any](reader func() (T, error), writer func(item T) error) MustItemReaderWriter[T] {
 	return &itemReaderWriter[T]{
 		read:  reader,
 		write: writer,
