6e5a31ab6dd0a43f96882fdc04a9a4ddb38e873d
murer <paulo.murer@infinity6.ai>
2026-09-04 14:05:32 -0300

    x

--- commonz/pathz/patternpathz/patternpathz_parse.go
+++ commonz/pathz/patternpathz/patternpathz_parse.go
@@ -2,25 +2,26 @@ package patternpathz
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/infinity6-ai/gox/commonz/errorz"
 	"github.com/infinity6-ai/gox/commonz/pathz"
 )
 
-func (p *PathPattern) Parse(pt *pathz.Path) (map[string]string, error) {
+func (p *PathPattern) Parse(pt *pathz.Path) (map[string]string, *pathz.Path, error) {
 	if p.original.Parents() != pt.Parents() {
-		return nil, fmt.Errorf("path parents mismatch, pattern: %s, path: %s", p, pt)
+		return nil, nil, fmt.Errorf("path parents mismatch, pattern: %s, path: %s", p, pt)
 	}
 
 	pParts := p.original.Parts()
 	ptParts := pt.Parts()
 
 	if len(pParts) > len(ptParts) {
-		return nil, fmt.Errorf("path length mismatch, pattern: %s, path: %s", p, pt)
+		return nil, nil, fmt.Errorf("path length mismatch, pattern: %s, path: %s", p, pt)
 	}
 
 	if len(pParts) == len(ptParts) && p.original.HasEndingSlash() && !pt.HasEndingSlash() {
-		return nil, fmt.Errorf("ending slash mismatch, pattern: %s, path: %s", p, pt)
+		return nil, nil, fmt.Errorf("ending slash mismatch, pattern: %s, path: %s", p, pt)
 	}
 
 	params := make(map[string]string)
@@ -28,11 +29,16 @@ func (p *PathPattern) Parse(pt *pathz.Path) (map[string]string, error) {
 		if name != "" {
 			params[name] = ptParts[i]
 		} else if pParts[i] != ptParts[i] {
-			return nil, fmt.Errorf("path mismatch at segment %d: expected '%s', got '%s'", i, pParts[i], ptParts[i])
+			return nil, nil, fmt.Errorf("path mismatch at segment %d: expected '%s', got '%s'", i, pParts[i], ptParts[i])
 		}
 	}
 
-	return params, nil
+	suffixParts := slices.Clone(ptParts[len(p.segments):])
+	if len(suffixParts) > 0 {
+		return params, pathz.New(pt.Parents(), suffixParts, pt.HasEndingSlash()), nil
+	}
+
+	return params, nil, nil
 }
 
 func (p *PathPattern) MustParse(pt *pathz.Path) map[string]string {
