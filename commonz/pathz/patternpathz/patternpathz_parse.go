0dc39e4290c87b2455984096b234020e82f3fe09
murer <paulo.murer@infinity6.ai>
2026-09-04 11:34:35 -0300

    x

--- commonz/pathz/patternpathz/patternpathz_parse.go
+++ commonz/pathz/patternpathz/patternpathz_parse.go
@@ -3,6 +3,7 @@ package patternpathz
 import "github.com/infinity6-ai/gox/commonz/pathz"
 
 func (p *PathPattern) Parse(pt *pathz.Path) (map[string]string, error) {
+	// path Parents must be equal p parents
 	panic("implement it")
 }
 
--- commonz/pathz/patternpathz/patternpathz_struct.go
+++ commonz/pathz/patternpathz/patternpathz_struct.go
@@ -3,7 +3,7 @@ package patternpathz
 import "github.com/infinity6-ai/gox/commonz/pathz"
 
 type PathPattern struct {
-	originalPattern *pathz.Path
+	originalPattern *pathz.Path    // a/{p1}/b/{p2}
 	segments        []string       // not empty on path param positions
 	paramNames      map[string]int // set of parameter names to its position
 }
