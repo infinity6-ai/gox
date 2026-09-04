a9764193e5958fb857e95eb34c6c454f0d6f92a8
murer <paulo.murer@infinity6.ai>
2026-09-04 12:09:31 -0300

    x

--- commonz/pathz/patternpathz/patternpathz_struct.go
+++ commonz/pathz/patternpathz/patternpathz_struct.go
@@ -1,17 +1,13 @@
 package patternpathz
 
 import (
-	"regexp"
-
 	"github.com/infinity6-ai/gox/commonz/pathz"
 )
 
-var paramValidator = regexp.MustCompile(`^{[a-zA-Z][a-zA-Z0-9]*}$`)
-
 type PathPattern struct {
-	originalPattern *pathz.Path    // a/{p1}/b/{p2}
-	segments        []string       // not empty on path param positions
-	paramNames      map[string]int // set of parameter names to its position
+	original *pathz.Path    // a/{p1}/b/{p2}
+	segments []string       // not empty on path param positions
+	names    map[string]int // set of parameter names to its position
 }
 
 func Parse(pattern *pathz.Path) (*PathPattern, error) {
@@ -22,5 +18,5 @@ func (p *PathPattern) String() string {
 	if p == nil {
 		return "<nil>"
 	}
-	return p.originalPattern.String()
+	return p.original.String()
 }
