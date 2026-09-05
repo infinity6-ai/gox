1d39cb848ccbe95a5a227960f6fd3f802e0c3843
murer <paulo.murer@infinity6.ai>
2026-09-05 10:46:53 -0300

    x

--- routez/routez/routez.go
+++ routez/routez/routez.go
@@ -13,36 +13,6 @@ import (
 	"github.com/infinity6-ai/gox/routez/internal/converter"
 )
 
-func parseRequest[P any, Q any, IH any, IB any, OH any, OB any](a *apiz.Api[P, Q, IH, IB, OH, OB], req *httpzrequest.Req, params map[string]string) *apiz.Req[P, Q, IH, IB] {
-	p, q, reqHeaders, reqBody := a.Zeros()
-	structjsonz.MustParseSingle(params, &p)
-	structjsonz.MustParse(req.Query, &q)
-	structjsonz.MustParse(converter.Header2Json(req.Headers), &reqHeaders)
-	jsonz.MustParseReader(req.Body, &reqBody)
-	return &apiz.Req[P, Q, IH, IB]{
-		PathParams:  p,
-		QueryParams: q,
-		ReqHeaders:  reqHeaders,
-		ReqBody:     reqBody,
-	}
-}
-
-func Register[P any, Q any, IH any, IB any, OH any, OB any](s *httpzserver.Server, apis ...*apiz.Api[P, Q, IH, IB, OH, OB]) {
-	for _, api := range apis {
-		s.AddHandler(api.Schema.Method, api.Schema.Path, func(ctx context.Context, resp httpzserver.Resp, req *httpzrequest.Req, params map[string]string) {
-			parsedReq := parseRequest(api, req, params)
-			parsedResp, err := api.Handler(ctx, parsedReq)
-			errorz.Check(err)
-			formattedHeaders := make(http.Header)
-			formattedHeaders.Set("Content-Type", "application/json")
-			mapRespHedaers := structjsonz.MustFormat(&parsedResp.RespHeaders)
-			converter.Json2Header(mapRespHedaers, formattedHeaders)
-			w := resp(parsedResp.Status, formattedHeaders)
-			jsonz.FormatWriter(w, parsedResp.RespBody)
-		})
-	}
-}
-
 func parseRequestV2[T apiz.ReqResp](a *apiz.ApiV2[T], req *httpzrequest.Req, params map[string]string) T {
 	reqResp := a.MewReqResp()
 	refs := reqResp.GetDataRefs()
