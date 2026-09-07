1915aea35b9be82da6e85b3175501ddb101a064f
murer <paulo.murer@infinity6.ai>
2026-09-07 12:22:31 -0300

    x

--- commonz/staticz/staticzloader/staticzloader_test.go
+++ commonz/staticz/staticzloader/staticzloader_test.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/infinity6-ai/gox/commonz/internal/stzfiles"
 	"github.com/infinity6-ai/gox/commonz/staticz/staticzloader"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUnitBasic(t *testing.T) {
-	staticzloader.GetCode(stzfiles.Name)
+	code := staticzloader.GetCode(stzfiles.Name)
+	require.NotEmpty(t, code)
 }
