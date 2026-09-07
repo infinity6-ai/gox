8bb5897924a6d9359be92e43fab1ea5cbe1984cb
murer <paulo.murer@infinity6.ai>
2026-09-07 11:45:05 -0300

    x

--- commonz/staticz/staticzloader/staticzloader.go
+++ commonz/staticz/staticzloader/staticzloader.go
@@ -1,5 +1,32 @@
 package staticzloader
 
-func SetCode(name any, tgz func() string) {
+import (
+	"fmt"
+	"sync"
 
+	"github.com/infinity6-ai/gox/commonz/encz/enczb64"
+	"github.com/infinity6-ai/gox/commonz/errorz"
+)
+
+type Codes struct {
+	codes map[any][]byte
+	mu    sync.RWMutex
+}
+
+var codes = &Codes{
+	codes: make(map[any][]byte),
+}
+
+func SetCode(name any, tgz func() string) {
+	decoded := decode(tgz)
+	codes.mu.Lock()
+	defer codes.mu.Unlock()
+	codes.codes[name] = decoded
+}
+
+func decode(tgz func() string) []byte {
+	encoded := tgz()
+	b, err := enczb64.StdDecode(encoded)
+	errorz.Check(fmt.Errorf("%w: error decoding", err))
+	return b.Bytes()
 }
