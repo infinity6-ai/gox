e3615119e9f08e3b96d1a207d99133ccfcca8102
murer <paulo.murer@infinity6.ai>
2026-09-07 17:36:27 -0300

    x

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 function cmd_comps_list() {
   # find . -maxdepth 2 -name go.mod | cut -d'/' -f2
   echo "versionz"
-  # echo "commonz"
+  echo "commonz"
   # echo "cryptz"
   # echo "schemaz"
   # echo "httpz"
