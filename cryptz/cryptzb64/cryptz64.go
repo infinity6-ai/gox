d04d2fba03f48ad9c44e684d895d028dda93dd52
murer <paulo.murer@infinity6.ai>
2026-09-07 11:00:32 -0300

    x

--- cryptz/cryptzb64/cryptz64.go
+++ cryptz/cryptzb64/cryptz64.go
@@ -1,49 +1,22 @@
 package cryptzb64
 
 import (
-	"encoding/base64"
-	"fmt"
-
 	"github.com/infinity6-ai/gox/commonz/constraintz/blobz"
+	"github.com/infinity6-ai/gox/commonz/encz/enczb64"
 )
 
-func newUrlEncoding() *base64.Encoding {
-	return base64.URLEncoding.WithPadding(base64.NoPadding)
-}
-
-func newStdEncoding() *base64.Encoding {
-	return base64.StdEncoding
-}
-
-func encode[T blobz.Data](enc *base64.Encoding, value T) blobz.Blob {
-	data := blobz.ToBytes(value)
-	buf := make([]byte, enc.EncodedLen(len(data)))
-	enc.Encode(buf, data)
-	return blobz.New(buf)
-}
-
-func decode[T blobz.Data](enc *base64.Encoding, value T) (blobz.Blob, error) {
-	data := blobz.ToBytes(value)
-	buf := make([]byte, enc.DecodedLen(len(data)))
-	l, err := enc.Decode(buf, data)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode base64: %w", err)
-	}
-	return blobz.New(buf[:l]), nil
-}
-
 func UrlEncode[T blobz.Data](value T) blobz.Blob {
-	return encode(newUrlEncoding(), value)
+	return enczb64.UrlEncode(value)
 }
 
 func UrlDecode[T blobz.Data](value T) (blobz.Blob, error) {
-	return decode(newUrlEncoding(), value)
+	return enczb64.UrlDecode(value)
 }
 
 func StdEncode[T blobz.Data](value T) blobz.Blob {
-	return encode(newStdEncoding(), value)
+	return enczb64.StdEncode(value)
 }
 
 func StdDecode[T blobz.Data](value T) (blobz.Blob, error) {
-	return decode(newStdEncoding(), value)
+	return enczb64.StdDecode(value)
 }
