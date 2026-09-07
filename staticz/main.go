75232f9d36d87655866f31e14195989e32573f69
murer <paulo.murer@infinity6.ai>
2026-09-07 10:37:22 -0300

    x

--- staticz/main.go
+++ staticz/main.go
@@ -37,8 +37,7 @@ func prepareGenerateCmd(ctx context.Context, parent *cobra.Command) {
 	}
 	cmd.PersistentFlags().String("imp", "github.com/infinity6-ai/gox/commonz/staticz", "import module")
 	cmd.PersistentFlags().String("code", "staticz.Instance().SetCode", "code")
-	cmd.PersistentFlags().String("dir", "dir", "")
-	cmd.MarkPersistentFlagRequired("dir")
+	cmd.PersistentFlags().String("dir", "dir", "stzfiles")
 
 	parent.AddCommand(cmd)
 }
