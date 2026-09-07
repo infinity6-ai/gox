5642171c5599de2cd951b332a611926ac8c69882
murer <paulo.murer@infinity6.ai>
2026-09-07 17:39:53 -0300

    x

--- comp.sh
+++ comp.sh
@@ -49,12 +49,20 @@ function cmd_release() {
     false
   fi
   git tag "$_tag"
-  git push origin "$_tag" 
+  git push origin "$_tag"
+  local _go_base_path="$(_go_base_path)"
+  local _go_dep_mod=""
+  GOWORK=off go list -mod=readonly -m "$_go_base_path/..." # | \
+    # grep "^$_go_base_path/.*\ v" | \
+    # cut -d' ' -f1 | while read _go_dep_mod; do
+    # go mod edit -require="${_go_dep_mod}@${_version}"
+  # done
+
 }
 
-# function _go_base_path() {
-#   GOWORK=off go list -m | rev | cut -d'/' -f2- | rev
-# }
+function _go_base_path() {
+  GOWORK=off go list -m | rev | cut -d'/' -f2- | rev
+}
 
 # function cmd_set_version() {
 #   local _version="${1?'_version'}"
