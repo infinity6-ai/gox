3a6f1a400510a896a63d13a0874f9f5ba8f316ba
murer <paulo.murer@infinity6.ai>
2026-09-09 09:38:06 -0300

    x

---
+++ routez/sampleroutez/routezsamplefraction/schemazsamplefraction_refs.go
@@ -0,0 +1,47 @@
+package routezsamplefraction
+
+import (
+	"github.com/infinity6-ai/gox/routez/apiz"
+)
+
+func (f *FractionReqResp) GetDataRefs() *apiz.DataRefs {
+	if f.Req == nil {
+		f.Req = &FractionReq{}
+	}
+	if f.Resp == nil {
+		f.Resp = &FractionResp{}
+	}
+	if f.Resp.Result == nil {
+		f.Resp.Result = &Result{}
+	}
+	return &apiz.DataRefs{
+		PathParams: &struct {
+			Numerator   *float64 `json:"numerator"`
+			Denumerator *float64 `json:"denumerator"`
+		}{
+			&f.Req.Numerator,
+			&f.Req.Denumerator,
+		},
+		QueryParams: &struct {
+			Precision *int `json:"precision"`
+		}{
+			&f.Req.Precision,
+		},
+		ReqHeaders: &struct {
+			TraceId *string `json:"trace_id"`
+		}{
+			&f.Req.TraceId,
+		},
+		ReqBody: &struct {
+			Reason *string `json:"reason"`
+		}{
+			&f.Req.Reason,
+		},
+		RespHeaders: &struct {
+			ReqId *string `json:"req_id"`
+		}{
+			&f.Resp.ReqId,
+		},
+		RespBody: f.Resp.Result,
+	}
+}
---
+++ routez/sampleroutez/routezsamplefraction/schemazsamplefraction_schema.go
@@ -0,0 +1,52 @@
+package routezsamplefraction
+
+import (
+	"github.com/infinity6-ai/gox/schemaz/schemaz"
+)
+
+func Schema() *schemaz.Api {
+	return &schemaz.Api{
+		Id: "samplefraction",
+
+		Desc: schemaz.Desc{
+			Name:     "Sample Fraction",
+			Summary:  "That is a sample of how to use this",
+			Markdown: "# Sample Fraction",
+		},
+
+		Method: "POST",
+		Path:   "/api/gox/routez/sample/fraction/{numerator}/{denumerator}",
+
+		ReqParams: []schemaz.Field{
+			{Name: "numerator", Desc: schemaz.Desc{Summary: "numerator"}, Spec: schemaz.Spec{Type: schemaz.TypeNumber}},
+			{Name: "denumerator", Desc: schemaz.Desc{Summary: "denumerator"}, Spec: schemaz.Spec{Type: schemaz.TypeNumber}},
+		},
+
+		ReqQuery: []schemaz.Field{
+			{Name: "precision", Desc: schemaz.Desc{Summary: "precision"}, Spec: schemaz.Spec{Type: schemaz.TypeNumber}},
+		},
+
+		ReqHeaders: []schemaz.Field{
+			{Name: "trace_id", Desc: schemaz.Desc{Summary: "trace id"}, Spec: schemaz.Spec{Type: schemaz.TypeString}},
+		},
+
+		ReqBody: &schemaz.Spec{
+			Type: schemaz.TypeObject,
+			Fields: []schemaz.Field{
+				{Name: "reason", Desc: schemaz.Desc{Summary: "reason"}, Spec: schemaz.Spec{Type: schemaz.TypeString}},
+			},
+		},
+
+		RespHeaders: []schemaz.Field{
+			{Name: "req_id", Desc: schemaz.Desc{Summary: "request id"}, Spec: schemaz.Spec{Type: schemaz.TypeString}},
+		},
+
+		RespBody: &schemaz.Spec{
+			Type: schemaz.TypeObject,
+			Fields: []schemaz.Field{
+				{Name: "display", Desc: schemaz.Desc{Summary: "fraction display"}, Spec: schemaz.Spec{Type: schemaz.TypeString}},
+				{Name: "result", Desc: schemaz.Desc{Summary: "fraction result"}, Spec: schemaz.Spec{Type: schemaz.TypeString}},
+			},
+		},
+	}
+}
