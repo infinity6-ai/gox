61db0652d2fd9d2fc53a9f4ac72740ba9ad7b097
murer <paulo.murer@infinity6.ai>
2026-09-10 09:54:50 -0300

    x

--- schemaz/mapperz/mapper.go
+++ schemaz/mapperz/mapper.go
@@ -1,6 +1,58 @@
 package mapperz
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/infinity6-ai/gox/commonz/errorz"
+)
+
 type Mapper struct {
 	Name   string
 	Target any
+}
+
+func (b *Mapper) UnmarshalJSON(data []byte) error {
+	// 1. Parse the incoming JSON into a map of raw byte slices
+	// var raw map[string]json.RawMessage
+	// if err := json.Unmarshal(data, &raw); err != nil {
+	// 	return err
+	// }
+
+	_, ok := b.Target.(Mapper)
+	if ok {
+		panic("mapper")
+	}
+
+	m, ok := b.Target.(map[string]*Mapper)
+	if ok {
+		var raw map[string]json.RawMessage
+		if err := json.Unmarshal(data, &raw); err != nil {
+			return err
+		}
+		for key, ble := range m {
+			if rawVal, exists := raw[key]; exists {
+				if err := json.Unmarshal(rawVal, ble.Target); err != nil {
+					return fmt.Errorf("error parsing key %q: %w", key, err)
+				}
+			}
+		}
+		return nil
+	}
+
+	err := json.Unmarshal(data, b.Target)
+	errorz.Check(err)
+	return err
+
+	// 2. Iterate over your defined mappings
+	// for key, ble := range b.m {
+	// 	// If the JSON payload contains this key, unmarshal it into the Target pointer
+	// 	if rawVal, exists := raw[key]; exists {
+	// 		if err := json.Unmarshal(rawVal, ble.Target); err != nil {
+	// 			return fmt.Errorf("error parsing key %q: %w", key, err)
+	// 		}
+	// 	}
+	// }
+
+	// return nil
 }
--- schemaz/mapperz/mapperz_test.go
+++ schemaz/mapperz/mapperz_test.go
@@ -10,33 +10,57 @@ import (
 
 func TestUnitBasic(t *testing.T) {
 
+	type Address struct {
+		Street string `json:"street"`
+		City   string `json:"city"`
+	}
+
 	type Person struct {
-		Name string
-		Age  int
+		Name               string   `json:"name"`
+		Age                int      `json:"age"`
+		MainAddress        *Address `json:"main_address"`
+		SecondaryAddresses *Address `json:"secondary_addresses"`
+		// Addresses   []Address `json:"addresses"`
 	}
 
 	expected := Person{
 		Name: "John",
 		Age:  30,
+		MainAddress: &Address{
+			Street: "123 Main St",
+			City:   "New York",
+		},
+		SecondaryAddresses: &Address{
+			Street: "456 Elm St",
+			City:   "Los Angeles",
+		},
 	}
 
 	var person Person
 
-	mapper := &mapperz.Mapper{
-		Target: map[string]any{
-			"name": &mapperz.Mapper{
+	mapper := mapperz.Mapper{
+		Target: map[string]*mapperz.Mapper{
+			"name": {
 				Name:   "name",
 				Target: &person.Name,
 			},
-			"age": &mapperz.Mapper{
+			"age": {
 				Name:   "age",
 				Target: &person.Age,
+			},
+			"main_address": {
+				Name:   "main_address",
+				Target: &person.MainAddress,
+			},
+			"secondary_addresses": {
+				Name:   "secondary_addresses",
+				Target: &person.SecondaryAddresses,
 			},
 		},
 	}
 
 	str := jsonz.MustFormat(expected)
-	jsonz.MustParse(str.Bytes(), mapper)
+	jsonz.MustParse(str.Bytes(), &mapper)
 
 	require.Equal(t, expected, person)
 }
