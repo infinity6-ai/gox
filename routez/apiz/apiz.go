c1e0f5e6afd9a2bbc13020fd7f0c4dadddb5affa
murer <paulo.murer@infinity6.ai>
2026-09-10 11:53:59 -0300

    x

--- routez/apiz/apiz.go
+++ routez/apiz/apiz.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/infinity6-ai/gox/commonz/validation/checker"
 	"github.com/infinity6-ai/gox/schemaz/schemaz"
+	"github.com/infinity6-ai/gox/schemaz/schemazv2"
 )
 
 type DataRefs struct {
@@ -19,6 +20,28 @@ type DataRefs struct {
 
 type ReqResp interface {
 	GetDataRefs() *DataRefs
+}
+
+type DataRefsV2 struct {
+	PathParams  *schemazv2.Schema
+	QueryParams *schemazv2.Schema
+	ReqHeaders  *schemazv2.Schema
+	ReqBody     *schemazv2.Schema
+	RespHeaders *schemazv2.Schema
+	RespBody    *schemazv2.Schema
+}
+
+type ReqRespV2 interface {
+	GetDataRefs() *DataRefsV2
+}
+
+type ApiV2[T ReqResp] struct {
+	Id      string
+	Desc    func() *schemazv2.Desc
+	Method  string
+	Path    string
+	Spec    T
+	Handler Handler[T]
 }
 
 type Handler[T ReqResp] func(ctx context.Context, reqResp T) (int, error)
--- schemaz/schemazv2/schemaz.go
+++ schemaz/schemazv2/schemaz.go
@@ -60,16 +60,3 @@ func (s *Schema) unmarshalJSONObject(data []byte) error {
 	}
 	return nil
 }
-
-type Api struct {
-	Id          string
-	Desc        func() *Desc
-	Method      string
-	Path        string
-	PathParams  *Schema
-	QueryParams *Schema
-	ReqHeaders  *Schema
-	ReqBody     *Schema
-	RespHeaders *Schema
-	RespBody    *Schema
-}
