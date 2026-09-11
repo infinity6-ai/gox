cc5dbd7d2bf788bf757a7fe73ba0b0f5dba432d1
murer <paulo.murer@infinity6.ai>
2026-09-10 23:14:34 -0300

    x

--- commonz/slicez/slicez.go
+++ commonz/slicez/slicez.go
@@ -1,5 +1,29 @@
 package slicez
 
-func GrowLen[S ~[]E, E any](s S, n int) S {
+func GrowLenBy[S ~[]E, E any](s S, n int) S {
+	panic("implement it")
+}
+
+func GrowLenTo[S ~[]E, E any](s S, n int) S {
+	panic("implement it")
+}
+
+func SetLen[S ~[]E, E any](s S, n int) S {
+	panic("implement it")
+}
+
+func Map[S ~[]I, I any, O any](s S, fn func(i int, v I) (O, bool, error)) ([]O, error) {
+	panic("implement it")
+}
+
+func MustMap[S ~[]I, I any, O any](s S, fn func(i int, v I) (O, bool)) []O {
+	panic("implement it")
+}
+
+func Filter[S ~[]I, I any](s S, fn func(i int, v I) (bool, error)) (S, error) {
+	panic("implement it")
+}
+
+func MustFilter[S ~[]I, I any](s S, fn func(i int, v I) bool) S {
 	panic("implement it")
 }
